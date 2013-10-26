5529de29e72274d00da69f77d565803bab3fdd0b
carlosroh <roh.race@gmail.com>
2013-10-26 13:15:02 -0700

    Primer commit. Creado el modelo y controlador de usuario, con sus respectivos tests.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RspecTests::Application.config.secret_key_base = '489aacda3410433aad20ec38b4ef9340f69ba5c4f78fa8680df8b76e243efec2ff5e6eba8850873cd11784efc9ced9ff365afe9845fa66d1abb301451047616e'
